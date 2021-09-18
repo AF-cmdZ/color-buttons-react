43841bc3e718027edb555c6226f6701883f78ff6
A Fish <ashley.paz.fish@gmail.com>
2021-09-18 14:02:06 -0500

    finished

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,9 +3,10 @@ import React, { useState } from "react";
 
 function Button(props) {
   const [count, setCount] = useState(0);
+  const styles = `${props.btnColor} m-4 p-4 rounded-md ${props.textColor}`;
   return (
     <button
-      className={`${props.btnColor} m-4 p-4 rounded-md ${props.textColor}`}
+      className={`${count > 3} ? ${styles} text-xl : ${styles}`}
       onClick={() => {
         setCount((prev) => prev + 1);
       }}
